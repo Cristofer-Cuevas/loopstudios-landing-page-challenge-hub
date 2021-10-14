47d486bac2735dbb323b0ba31cc4696d343aa660
Christopher <cristoferperezmejia80@gmail.com>
2021-10-14 18:28:42 -0400

    project set up

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Loopstudios from "./Loopstudios/Loopstudio";
+
+function App() {
+  return (
+    <>
+      <Loopstudios />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Loopstudios/Loopstudio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./assets/styles/Loopstudios.css";
+
+import "./assets/styles/normalize.css";
+
+const Loopstudios = () => {
+  return <div>hola</div>;
+};
+
+export default Loopstudios;
