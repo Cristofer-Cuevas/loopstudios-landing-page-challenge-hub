53ccd2b0eac5a321940173e758bb36b935fb5148
Christopher <cristoferperezmejia80@gmail.com>
2021-11-01 15:29:47 -0400

    header finished

--- src/components/Loopstudios/Loopstudio.js
+++ src/components/Loopstudios/Loopstudio.js
@@ -1,11 +1,37 @@
 import React from "react";
 
+import logo from "./assets/images/logo.svg";
+import hamburger from "./assets/images/icon-hamburger.svg";
 import "./assets/styles/Loopstudios.css";
 
 import "./assets/styles/normalize.css";
 
-const Loopstudios = () => {
-  return <div>hola</div>;
+const Loopstudios = (props) => {
+  return (
+    <>
+      <header className="headerContainer">
+        <div className="logoHamburgerContainer">
+          <img className="logo" src={logo} alt="logo" />
+          <img className="hamburger" src={hamburger} alt="menu" />
+        </div>
+
+        <h1 className="title">
+          IMMERSIVE EXPERIENCES <span className="that"> THAT</span> DELIVER
+        </h1>
+      </header>
+      <main>
+        <Creations creations={props.creations} />
+      </main>
+    </>
+  );
 };
+
+const Creations = (props) => {
+  return props.creations.map((creation) => {
+    return <h3>{creation.title}</h3>;
+  });
+};
+
+// const Footer = () => {};
 
 export default Loopstudios;
