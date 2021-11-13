b1340eb006d4461e130357971d93133131cad32b
Christopher <cristoferperezmejia80@gmail.com>
2021-11-13 17:35:46 -0400

    h

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Loopstudios from "./Loopstudios/Loopstudio";
 
+const creations = [{ title: "deep earth" }, { title: "night arcade" }, { title: "soccer team vr" }, { title: "the grid" }, { title: "from up above vr" }, { title: "pocket borealis" }, { title: "the curiosity" }, { title: "make it fisheye" }];
+
 function App() {
   return (
     <>
-      <Loopstudios />
+      <Loopstudios creations={creations} />
     </>
   );
 }
